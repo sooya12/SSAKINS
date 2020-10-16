03b76a4239f4226850942da36e369b35e8f22b08
1000min <o_ogog@naver.com>
2020-10-16 10:27:41 +0900

    init

---
+++ backend/src/main/java/com/ssafy/ssakins/SsakinsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssakins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsakinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsakinsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ssafy/ssakins/SsakinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssakins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsakinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
