74c7dc65e591aaafabfcf7d7bf38c6525f6fe492
JINSUB <neyo0311@naver.com>
2020-10-28 17:27:26 +0900

    FE | 메인 페이지 진행중 | S03P31A201-49

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,53 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <div>
+    <div class = "navigator" style="float:left; margin-top:16px;">
+    <v-card
+        height="400"
+        width="256"
+        class="mx-auto"
+    >
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+    </div>
+
+    <div style="float:left;">
+      <router-view/>
+    </div>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -9,18 +55,11 @@
 export default {
   name: 'App',
   components: {
-    
-  },
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '@/views/Main.vue'
+import test from '@/views/test.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes =[
         path : '/main',
         name : 'Main',
         component : Main
+    },
+    {
+        path : '/test',
+        name : 'test',
+        component : test
     }
 ]
 
