56d7e89eba379c1298f63b7284b523c35310b8fa
JINSUB <neyo0311@naver.com>
2020-10-28 12:34:11 +0900

    FE | 로그인 화면 진행

---
+++ frontend/src/router/index.js
@@ -0,0 +1,15 @@
+// import { hasOwnMetadata } from 'core-js/fn/reflect'
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+
+// Vue.use(VueRouter)
+
+// const routes = [
+//     {
+//         path : '/',
+//         name : 'home',
+//         component : Home
+//     }
+// ]
+
+// export default router
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>홈</p>
+</template>
