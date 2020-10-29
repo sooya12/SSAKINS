ea9fa27d404be8230a52c2bb7ac92d6cd10e4765
Chaeyeong <lcy707@naver.com>
2020-10-29 16:08:23 +0900

    [BE] update | 몽고디비 연동 중 오류발생 | S03P31A201-53

---
+++ backend/src/main/java/com/ssafy/ssakins/config/MongoConfig.java
@@ -0,0 +1,16 @@
+//package com.ssafy.ssakins.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.config.EnableMongoAuditing;
+//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+//
+//import com.mongodb.client.MongoClient;
+//
+//@Configuration
+//@EnableMongoRepositories()
+//@EnableMongoAuditing
+//public class MongoConfig {
+//
+//	
+//	
+//}
--- backend/src/main/java/com/ssafy/ssakins/dto/accountDTO.java
+++ backend/src/main/java/com/ssafy/ssakins/dto/accountDTO.java
@@ -1,5 +1,46 @@
 package com.ssafy.ssakins.dto;
 
-public class accountDTO {
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="account")
+public class accountDTO {
+	
+	@Id
+	private String name;
+	private String email;
+	private String id;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 }
