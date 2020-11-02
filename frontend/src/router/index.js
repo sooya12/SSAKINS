c00f2026d354aecf8da40e90a67a8736813c5790
JINSUB <neyo0311@naver.com>
2020-11-02 15:38:18 +0900

    [FE] update | 개발자의 숨터 추가중

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,24 +3,30 @@ import VueRouter from 'vue-router'
 import Main from '@/views/Main.vue'
 import Notice from '@/views/Notice.vue'
 import Create from '@/views/Create.vue'
+import Game from '@/views/Game.vue'
 
 Vue.use(VueRouter)
 
 const routes =[
     {
         path : '/main',
-        name : 'main',
+        name : 'Main',
         component : Main
     },
     {
         path : '/notice',
-        name : 'notice',
+        name : 'Notice',
         component : Notice
     },
     {
         path : '/create',
-        name : 'create',
+        name : 'Create',
         component : Create
+    },
+    {
+        path : '/game',
+        name : 'Game',
+        component : Game
     },
 ]
 
