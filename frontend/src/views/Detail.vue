11b48075cf7690597fbafa5bd8aaa4e006becbe5
Lee Yujin <ajph03102@naver.com>
2020-11-09 13:43:09 +0900

    [FE] update | 설정 상세 화면 헤더/네비게이션 추가 | S03P31A201-51

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -1,52 +1,66 @@
 <template>
-  <v-container id="detail">
-    <div id="title">
-      <h2>CI/CD 설정 상세</h2>
+  <v-container id="contianer">
+    <div>
+      <headers></headers>
     </div>
-    <v-simple-table dense>
-      <template v-slot:default>
-        <tbody>
-          <tr>
-            <td class="text-left font15" style="width: 8vw">
-              설정 CI/CD 명
-            </td>
-            <td class="text-left font15" style="width: 40vw">
-              {{ name }}
-            </td>
-            <td class="font15" style="width: 5vw">생성일</td>
-            <td class="font15" style="width: 6vw">{{ createDay }}</td>
-            <td class="font15" style="width: 5vw">수정일</td>
-            <td class="font15" style="width: 6vw">{{ editDay }}</td>
-          </tr>
-          <tr>
-            <td class="text-left font15">설정 내용</td>
-            <td colspan="5">
-              <div id="content" c class="font15">
-              컴포넌트들이 들어갈 자리입니다.
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-    <div id="btn-area">
-      <v-btn color="grey lighten-3" style="float:left; margin-left:20px;">삭제하기</v-btn>
-      <v-btn elevation="2" color="grey darken-3" style="color: white; float:right; margin-right:20px" @click="goEdit">수정하기</v-btn>
+    <div
+      id="navigator"
+      style="float:left; margin-top:16px; margin-right:20px; margin-left:20px;"
+    >
+      <navigator></navigator>
+    </div>
+    <div id="Edit">
+      <div id="title">
+        <h2>CI/CD 설정 상세</h2>
+      </div>
+      <v-simple-table dense>
+        <template v-slot:default>
+          <tbody>
+            <tr>
+              <td class="text-left font15" style="width: 8vw">
+                설정 CI/CD 명
+              </td>
+              <td class="text-left font15" style="width: 40vw">
+                {{ name }}
+              </td>
+              <td class="font15" style="width: 5vw">생성일</td>
+              <td class="font15" style="width: 6vw">{{ createDay }}</td>
+              <td class="font15" style="width: 5vw">수정일</td>
+              <td class="font15" style="width: 6vw">{{ editDay }}</td>
+            </tr>
+            <tr>
+              <td class="text-left font15">설정 내용</td>
+              <td colspan="5">
+                <div id="content" c class="font15">
+                컴포넌트들이 들어갈 자리입니다.
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <div id="btn-area">
+        <v-btn color="grey lighten-3" style="float:left; margin-left:20px;">삭제하기</v-btn>
+        <v-btn elevation="2" color="grey darken-3" style="color: white; float:right; margin-right:20px" @click="goEdit">수정하기</v-btn>
+      </div>
     </div>
   </v-container>
 </template>
 
 <script>
+import Navigator from "../components/Navigator";
+import Header from "../components/Header";
+
 export default {
   name: 'Detail',
   data() {
@@ -60,12 +74,26 @@ export default {
     goEdit() {
       this.$router.push("/edit")
     }
+  },
+  components: {
+    navigator: Navigator,
+    headers: Header
   }
   
 }
 </script>
 
 <style scoped>
+#container {
+  width: 100%;
+  min-width: 100px;
+}
+
+#navigator {
+  width: 20%;
+  min-width: 100px;
+}
+
 #detail {
     text-align: center;
     width: 70vw;
