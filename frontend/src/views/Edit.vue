4e3bca2d882951068036e36c168744e854ed9376
Lee Yujin <ajph03102@naver.com>
2020-11-09 13:39:16 +0900

    [FE] update | 설정 수정 화면 헤더/네비게이션 추가 | S03P31A201-52

--- frontend/src/views/Edit.vue
+++ frontend/src/views/Edit.vue
@@ -1,47 +1,61 @@
 <template>
-  <v-container id="edit">
-    <div id="title">
-      <h2>CI/CD 설정 수정</h2>
+  <v-container id="contianer">
+    <div>
+      <headers></headers>
     </div>
-    <v-simple-table dense>
-      <template v-slot:default>
-        <tbody>
-          <tr>
-            <td class="text-left font15" style="width: 8vw">
-              설정 CI/CD 명
-            </td>
-            <td class="text-left font15" style="width: 62vw">
-              <v-text-field  v-model="name" class="font15" placeholder="내용을 입력해주세요" hide-details="auto" dense filled shaped></v-text-field>
-            </td>
-          </tr>
-          <tr>
-            <td class="text-left font15">설정 내용</td>
-            <td>
-              <div id="content" class="font15">
-              컴포넌트들이 들어갈 자리입니다.
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              <p>아</p>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-    <div id="btn-area">
-      <v-btn elevation="2" color="grey darken-3" style="color: white">저장하기</v-btn>
+    <div
+      id="navigator"
+      style="float:left; margin-top:16px; margin-right:20px; margin-left:20px;"
+    >
+      <navigator></navigator>
+    </div>
+    <div id="create">
+      <div id="title">
+        <h2>CI/CD 설정 수정</h2>
+      </div>
+      <v-simple-table dense>
+        <template v-slot:default>
+          <tbody>
+            <tr>
+              <td class="text-left font15" style="width: 8vw">
+                설정 CI/CD 명
+              </td>
+              <td class="text-left font15" style="width: 62vw">
+                <v-text-field  v-model="name" class="font15" placeholder="내용을 입력해주세요" hide-details="auto" dense filled shaped></v-text-field>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-left font15">설정 내용</td>
+              <td>
+                <div id="content" class="font15">
+                컴포넌트들이 들어갈 자리입니다.
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                <p>아</p>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <div id="btn-area">
+        <v-btn elevation="2" color="grey darken-3" style="color: white">저장하기</v-btn>
+      </div>
     </div>
   </v-container>
 </template>
 
 <script>
+import Navigator from "../components/Navigator";
+import Header from "../components/Header";
+
 export default {
   name: 'Edit',
   data() {
@@ -49,11 +63,25 @@ export default {
       name: 'SSAKINS 1차 CI/CD 설정',
     
     }
+  },
+  components: {
+    navigator: Navigator,
+    headers: Header
   }
 }
 </script>
 
 <style scoped>
+#container {
+  width: 100%;
+  min-width: 100px;
+}
+
+#navigator {
+  width: 20%;
+  min-width: 100px;
+}
+
 #edit {
     text-align: center;
     width: 70vw;
