24626721ed6af14db106177db28725434d62de57
최현수 <hyeonsu2014@naver.com>
2020-11-09 21:08:35 +0900

    [BE] initial | Project 정보 정리해서 sh 파일로 다운로드 | S03P31P201-94

--- backend/src/main/java/com/ssafy/ssakins/controller/MongoTestController.java
+++ backend/src/main/java/com/ssafy/ssakins/controller/MongoTestController.java
@@ -1,15 +1,14 @@
 package com.ssafy.ssakins.controller;
 
 import com.ssafy.ssakins.dto.AccountAndProject;
-import com.ssafy.ssakins.entity.Account;
-import com.ssafy.ssakins.entity.Git;
-import com.ssafy.ssakins.entity.Project;
+import com.ssafy.ssakins.entity.*;
 import com.ssafy.ssakins.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,6 +42,77 @@ class MongoTestController {
     @RequestMapping(value = "/select/{email}", method = RequestMethod.GET)
     public ResponseEntity select(@PathVariable("email") String email) {
         return ResponseEntity.ok().body(accountRepository.findByEmail(email).get());
+    }
+
+    @RequestMapping(value = "/infoFile/{email}/{projectName}", method = RequestMethod.GET)
+    ResponseEntity infoFile(@PathVariable String email, @PathVariable String projectName) throws FileNotFoundException {
+        InputStreamResource isr = new InputStreamResource(new FileInputStream(getInfoFile(email, projectName)));
+        return ResponseEntity.ok().header("Content-Disposition", "attachment; filename=\"accountInfo.sh\"").body(isr);
+    }
+
+    public File getInfoFile(String email, String projectName) {
+        Account account = accountRepository.findByEmail(email).get();
+
+//        String fileName = "accountInfo.sh";
+        File file = new File("accountInfo"); // 파일 객체 생성
+
+        try {
+            FileWriter fw = new FileWriter(file, false); // 기존 내용에 이어서 작성
+            Project project = new Project();
+
+            for (Project p : account.getProject()) {
+                if(projectName.equals(p.getName())) {
+                    project = p;
+                    break;
+                }
+
+                project = null;
+            }
+
+            if(!project.equals(null)) {
+                fw.write("PROJECTNAME=" + project.getName() + "\n");
+                fw.write("URL=" + project.getUrl() + "\n");
+                fw.write("PORT=" + project.getPort() + "\n");
+                
+                Git git = project.getGit();
+                
+                fw.write("GITID=" + git.getId() + "\n");
+                fw.write("GITUSERNAME=" + git.getName() + "\n");
+                fw.write("GITPASSWORD=" + git.getPassword() + "\n");
+                fw.write("GITURL=" + git.getGiturl() + "\n");
+                fw.write("GITTYPE=" + git.getType() + "\n");
+                fw.write("GITCREDENTIAL=" + git.getId() + "\n"); // git-configuration.xml - CredentialsId
+
+                SSHServer sshServer = project.getSshServer();
+
+                fw.write("SSHNAME=" + sshServer.getName() + "\n");
+                fw.write("IPADDRESS=" + sshServer.getHostName() + "\n");
+                fw.write("SERVERUSERNAME=" + sshServer.getUserName() + "\n");
+                fw.write("SERVERPASSWORD=" + sshServer.getPassword() + "\n");
+                fw.write("PEMKEY=" + sshServer.getKey() + "\n");
+                fw.write("REMOTEDIRECTORY=" + sshServer.getRemoteDirectory() + "\n");
+
+                Server server = project.getServers();
+
+                if("Vue".equals(server.getKind())) {
+                    fw.write("FRONTNAME=" + server.getName() + "\n");
+                    fw.write("FRONTLOCATION=" + server.getInfo() + "\n");
+                    fw.write("FRONTPORT=" + server.getPort() + "\n");
+                } else if("Spring".equals(server.getKind())) {
+                    fw.write("BACKNAME=" + server.getName() + "\n");
+                    fw.write("BACKLOCATION=" + server.getInfo() + "\n");
+                    fw.write("BACKPORT=" + server.getPort() + "\n");
+                }
+            }
+
+            fw.flush();
+            fw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return file;
     }
 
 }
--- backend/src/main/java/com/ssafy/ssakins/entity/Project.java
+++ backend/src/main/java/com/ssafy/ssakins/entity/Project.java
@@ -3,12 +3,14 @@ package com.ssafy.ssakins.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Project {
     private String name;
