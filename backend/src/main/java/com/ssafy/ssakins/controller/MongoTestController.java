147c219da2b32b0b335c0fccf2103974e89d726a
최현수 <hyeonsu2014@naver.com>
2020-11-09 16:04:56 +0900

    [BE] update | Account 삽입 | S03P31P201-54

--- backend/src/main/java/com/ssafy/ssakins/controller/MongoTestController.java
+++ backend/src/main/java/com/ssafy/ssakins/controller/MongoTestController.java
@@ -1,12 +1,12 @@
 package com.ssafy.ssakins.controller;
 
+import com.ssafy.ssakins.dto.AccountAndProject;
 import com.ssafy.ssakins.entity.Account;
+import com.ssafy.ssakins.entity.Git;
+import com.ssafy.ssakins.entity.Project;
 import com.ssafy.ssakins.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,9 +20,17 @@ class MongoTestController {
     @Autowired
     AccountRepository accountRepository;
 
-    @RequestMapping(value="/selectAll", method = RequestMethod.GET)
-    public void selectAll() throws IOException {
+    @RequestMapping(value = "/selectAll", method = RequestMethod.GET)
+    public void selectAll() {
         System.out.println(accountRepository.findAll());
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public void insert(@RequestBody AccountAndProject accountAndProject) {
+        System.out.println(accountRepository.findByEmail(accountAndProject.getUserEmail()));
+        Account account = accountRepository.findByEmail(accountAndProject.getUserEmail()).get();
+        account.addProject(accountAndProject.getProject());
+        accountRepository.save(account);
     }
 
 }
