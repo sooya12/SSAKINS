1cde8934c49d02bad13f3befedee4103c2acf86f
Lee Yujin <ajph03102@naver.com>
2020-11-13 12:39:20 +0900

    [FE] initial | 로그아웃 컴포넌트 추가

---
+++ frontend/src/components/LogoutComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "LogoutComponent.vue",
+  mounted() {
+    this.$router.push('/')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
